39f6406cb53998fa97b8fee60c1e2fceb2de3b8f
krushstar34 <KUSHAJ.34159@stu.upes.ac.in>
2026-09-11 16:17:55 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,34 @@
+//Q34: Write a program to check if a number is prime.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Prime
+
+Input 2:
+10
+Output 2:
+Not prime
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int n,count =0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+    if(count==2)
+    printf("%d is a prime number",n);
+    else
+    printf("%d is not a prime number",n);
+
+    return 0;
+}
