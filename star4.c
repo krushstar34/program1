76b2d43a55ef6334a4a976ddf670c39e91e79af1
krushstar34 <KUSHAJ.34159@stu.upes.ac.in>
2026-09-19 11:06:36 +0530

    Add files via upload

---
+++ star4.c
@@ -0,0 +1,50 @@
+//Q52: Write a program to print the following pattern:
+
+/*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+    for( i =1;i<=5;i++){
+        int n;
+        if(i<=3)
+        n = 2*i -1;
+        else
+        n = 2*(5-i) +1;
+        for(j=1;j<=n;j++){
+            printf("\n");
+        }
+        if(i!=5)
+        printf("\n");
+    }
+    return 0;
+}
